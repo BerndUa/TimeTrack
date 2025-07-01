d26724d1b9d61c8cfd217fb26f39a888833858b5
Marc Burk <mburk@luminet.de>
2025-07-01 14:01:40 +0200

    add addPause and stop to trackingcontroller

--- Server/TimeTRackApi.TrackingController.pas
+++ Server/TimeTRackApi.TrackingController.pas
@@ -1,13 +1,41 @@
-unit TimeTRackApi.TrackingController;
+unit TimeTrackApi.TrackingController;
 
 interface
 
 uses
-  MVCFramework, MVCFramework.Commons, MVCFramework.Nullables, MVCFramework.Serializer.Commons, System.Generics.Collections;
+  MVCFramework,
+  MVCFramework.Commons,
+  MVCFramework.Nullables,
+  MVCFramework.Serializer.Commons,
+  MVCFramework.Swagger.Commons,
+  System.Generics.Collections;
 
 type
-  [MVCPath('/api')]
+
+  TTimeEntries = class(TObject);
+  TTimeEntry = class(TObject);
+
+  [MVCPath('/api/tracking')]
   TTrackingController = class(TMVCController)
+  public
+    [MVCPath('/stop')]
+    [MVCHTTPMethod([httpPUT])]
+    [MVCDoc('Stop time tracking')]
+    [MVCSwagSummary('Tracking', 'stop time tracking for an Time Entry', 'addPause')]
+    [MVCSwagParam(plBody, 'TimeEntryId', 'Time Entry ID', ptInteger, True)]
+    [MVCSwagResponses(200, 'TimeEntry', TTimeEntry)]
+    [MVCSwagResponses(404, 'Time Entry not found', TMVCErrorResponse)]
+    procedure Stop(ATimeEntryId: Integer);
+
+    [MVCPath('/addPause')]
+    [MVCHTTPMethod([httpPUT])]
+    [MVCDoc('Add pause to an time entry')]
+    [MVCSwagSummary('Tracking', 'Add Pause to an Time Entry', 'addPause')]
+    [MVCSwagParam(plBody, 'TimeEntryId', 'Time Entry ID', ptInteger, True)]
+    [MVCSwagParam(plBody, 'PauseMinutes', 'Value for Pause in Minutes', ptInteger, True)]
+    [MVCSwagResponses(200, 'TimeEntry', TTimeEntry)]
+    [MVCSwagResponses(404, 'Time Entry not found', TMVCErrorResponse)]
+    procedure AddPause(ATimeEntryId: Integer; APauseMinutes: Integer);
   protected
     procedure OnBeforeAction(Context: TWebContext; const AActionName: string; var Handled: Boolean); override;
     procedure OnAfterAction(Context: TWebContext; const AActionName: string); override;
@@ -18,8 +46,19 @@ type
 implementation
 
 uses
-  System.StrUtils, System.SysUtils, MVCFramework.Logger;
+  System.StrUtils,
+  System.SysUtils,
+  MVCFramework.Logger;
 
+procedure TTrackingController.AddPause(ATimeEntryId: Integer; APauseMinutes: Integer);
+begin
+  //const TimeEntryId = Context.Request.Params['TimeEntryId'].ToInteger;
+  //const Pause = Context.Request.Params['PauseMinutes'].ToInteger;
+  //const TimeEntry = TimeEntryies.ById(ATimeEntryId);
+  //TimeEntry.AddPause(Pause);
+  //Render(TimeEntry);
+  // Is Tracking Running?
+end;
 
 procedure TTrackingController.OnAfterAction(Context: TWebContext; const AActionName: string);
 begin
@@ -35,5 +74,10 @@ begin
   inherited;
 end;
 
+procedure TTrackingController.Stop(ATimeEntryId: Integer);
+begin
+  // TBD
+  // Is Tracking Running?
+end;
 
 end.
